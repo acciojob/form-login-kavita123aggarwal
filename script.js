73ca7a6a86b48eb3d835391c804b1626fd047165
kavita123aggarwal <118376629+kavita123aggarwal@users.noreply.github.com>
2023-08-23 22:24:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+const form = document.getElementById("form1");
 function getFormvalue() {
-    //Write your code here
+	let fullName = form["fname"].value + " "+form["lname"].value;
+		alert(fullName);
 
 }
